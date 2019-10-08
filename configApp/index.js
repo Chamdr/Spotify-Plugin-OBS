245091c7dd9ce0d620667693dc0c497bbff31db5
Gwilherm Folliot <gwilherm55fo@gmail.com>
2019-10-08 20:17:07 +0200

    fixed bug: access_token was not updated

--- configApp/index.js
+++ configApp/index.js
@@ -187,12 +187,7 @@ setInterval(() => {
 
         request.post(authOptions, function (error, response, body) {
             if (!error && response.statusCode === 200) {
-                const access_token = body.access_token;
-                const obj = {
-                    token: access_token
-                }
-                console.log(access_token)
-                fs.writeFile(path.join(__dirname, "..", "plugin", "config.json"), JSON.stringify(obj), (err) => { if (err) console.error(err) })
+                 access_token = body.access_token
             }
         })
     }
